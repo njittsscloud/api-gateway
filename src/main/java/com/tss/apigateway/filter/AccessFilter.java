267b334905338584b6c2ca54666d45e54216d419
MQG <mo1056610188@163.com>
2018-12-09 16:43:20 +0800

    返回json

--- src/main/java/com/tss/apigateway/filter/AccessFilter.java
+++ src/main/java/com/tss/apigateway/filter/AccessFilter.java
@@ -2,15 +2,20 @@ package com.tss.apigateway.filter;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
+import com.tss.basic.common.util.JsonUtil;
+import com.tss.basic.site.response.DefaultResponse;
+import com.tss.basic.site.response.ErrorDataResponse;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 import org.springframework.cloud.netflix.zuul.util.ZuulRuntimeException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Component
 public class AccessFilter extends ZuulFilter {
@@ -60,6 +65,16 @@ public class AccessFilter extends ZuulFilter {
     private void return401(RequestContext ctx) {
         LOG.warn("access token is empty");
         ctx.setSendZuulResponse(false);
-        ctx.setResponseStatusCode(401);
+        ctx.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+        ctx.setResponseBody(this.getResponseBody(HttpStatus.UNAUTHORIZED.value(), "user not login"));
+        ctx.getResponse().setContentType("application/json");
+    }
+
+    private String getResponseBody(int code, String msg) {
+        ErrorDataResponse response = new ErrorDataResponse();
+        response.setMsg(msg);
+        response.setSuccess(false);
+        response.setErrorCode(code);
+        return JsonUtil.obj2json(response);
     }
 }
