582d4c3eb4a43f4aaf6111a21b27d18becae29bb
MQG <mo1056610188@163.com>
2018-12-09 16:43:29 +0800

    返回json

--- src/main/java/com/tss/apigateway/filter/AccessFilter.java
+++ src/main/java/com/tss/apigateway/filter/AccessFilter.java
@@ -3,7 +3,6 @@ package com.tss.apigateway.filter;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.tss.basic.common.util.JsonUtil;
-import com.tss.basic.site.response.DefaultResponse;
 import com.tss.basic.site.response.ErrorDataResponse;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -15,7 +14,6 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Component
 public class AccessFilter extends ZuulFilter {
